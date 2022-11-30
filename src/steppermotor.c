17eef2bc676f5d84db60aa96ac5824c8bd6780be
Arellano Jann <89598304+Arellano-Jann@users.noreply.github.com>
2022-11-29 20:11:29 -0800

    Stepper motor files made and partly done

---
+++ src/steppermotor.c
@@ -0,0 +1,3 @@
+// Write a stepper motor function, it's super easy. I think one pin is for vcc, one for ground, one is direction and the last you just pulse for every tick you want to move
+// https://components101.com/motors/28byj-48-stepper-motor
+// https://create.arduino.cc/projecthub/debanshudas23/getting-started-with-stepper-motor-28byj-48-3de8c9
